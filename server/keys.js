b41ed55add88a9a815af5613765f79b2d4a9558f
Victor Nunez <VN135766@gmail.com>
2022-09-02 20:57:04 -0400

    signup working route

---
+++ server/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+  MONGOURI:"mongodb+srv://VN135766:Tx8_t8-X9rH!SB.@cluster0.ph9uzfr.mongodb.net/?retryWrites=true&w=majority"
+}
---
+++ server/routes/auth.js
@@ -0,0 +1,18 @@
+const express = require ("express")
+const router = express.Router()
+
+
+router.get("/",(req,res)=>{
+  res.send("hello")
+})
+
+router.post('/signup',(req,res)=>{
+const {name,email,password} = req.body 
+if (!email || !password || !name) {
+  res.json({error:"please add all the fields"})
+}
+return res.status(422).json({message:"successfully posted"})
+})
+
+
+module.exports = router
