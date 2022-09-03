66ec538404add095cd076be7c225b4c813ad6006
Victor Nunez <VN135766@gmail.com>
2022-09-02 21:29:15 -0400

    hashed pass now working

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,18 +1,41 @@
-const express = require ("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const bcrypt = require("bcryptjs");
 
+router.post("/signup", (req, res) => {
+  const { name, email, password } = req.body;
+  if (!email || !password || !name) {
+    return res.status(422).json({ error: "please add all the fields" });
+  }
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (savedUser) {
+        return res
+          .status(422)
+          .json({ error: "user already exists with that email" });
+      }
+      bcrypt.hash(password, 12).then((hashedpassword) => {
+        const user = new User({
+          email,
+          password: hashedpassword,
+          name,
+        });
 
-router.get("/",(req,res)=>{
-  res.send("hello")
-})
+        user
+          .save()
+          .then((user) => {
+            res.json({ message: "saved successfully" });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-router.post('/signup',(req,res)=>{
-const {name,email,password} = req.body 
-if (!email || !password || !name) {
-  res.json({error:"please add all the fields"})
-}
-return res.status(422).json({message:"successfully posted"})
-})
-
-
-module.exports = router
+module.exports = router;
